d3f24e1420d742c656e8c44759e7e932de11a0c6
JosiahFuller <josiahfullervgdesign@gmail.com>
2022-01-26 09:32:05 -0800

    Created Haunted Mansion

---
+++ Haunted Mansion/Haunted Mansion Program/Haunted Mansion.js
@@ -0,0 +1,7 @@
+/*
+Programmer: Josiah Fuller
+Team Member: Spencer
+Date: 1.26.2022
+Program: We will be making a program to go through a haunted mansion
+ filled with haunted sounds, spooky color changes, and other obstacles
+*/
