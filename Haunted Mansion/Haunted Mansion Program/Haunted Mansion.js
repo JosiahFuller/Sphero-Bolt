5e891fa145a3bdaf99939c65a96f0c6f2111749e
JosiahFuller <josiahfullervgdesign@gmail.com>
2022-01-27 09:52:56 -0800

    Josiah - Function "Go To Blue Light": Stable

--- Haunted Mansion/Haunted Mansion Program/Haunted Mansion.js
+++ Haunted Mansion/Haunted Mansion Program/Haunted Mansion.js
@@ -5,3 +5,16 @@ Date: 1.26.2022
 Program: We will be making a program to go through a haunted mansion
  filled with haunted sounds, spooky color changes, and other obstacles
 */
+
+async function startProgram()
+{
+	await goToBlueLight();
+	
+}
+
+async function goToBlueLight()
+{
+	await scrollMatrixText('Start', {r: 255, g: 0, b: 0}, 30, true);
+	await roll(0, 100, 1.45);
+	await setMainLed({ r: 0, g: 0, b: 255 });
+}
