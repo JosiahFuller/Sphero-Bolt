4072b17136b222b426ce960b078912fec7a7d0fd
JosiahFuller <josiahfullervgdesign@gmail.com>
2022-01-31 10:00:38 -0800

    Josiah - Function "Go To Red Light" Attempted, Failed (1/31/2022)

--- Haunted Mansion/Haunted Mansion Program/Haunted Mansion.js
+++ Haunted Mansion/Haunted Mansion Program/Haunted Mansion.js
@@ -7,23 +7,29 @@ Program: We will be making a program to go through a haunted mansion
 */
 
 async function startProgram(){
-	//Calls the individual functions
 	await goToBlueLight();
 	await goSound1();
+	await goRedLight();
 }
 
 async function goToBlueLight(){
-	//Display the Start message
 	await scrollMatrixText('Start', {r: 255, g: 0, b: 0}, 30, true);
-	//Roll to the blue light
 	await roll(0, 50, 3.1);
-	//Sets the light to blue
 	await setMainLed({ r: 0, g: 0, b: 255 });
 }
 
 async function goSound1(){
 	await delay(0.5);
-	//Rolls to the sound icon and plays a sound
 	await roll(90, 50, 2.1);
-	await Sound.Mechanical.Ambulance.play(true);
+	await Sound.Mechanical.Ambulance.play(false);
+}
+
+async function goRedLight()
+{
+	await delay(0.5);
+	await roll(180, 50, 1.2);
+	await roll(220, 50, 1.5);
+	await roll(130, 50, 0.75);
+	await setMainLed({ r: 255, g: 0, b: 0 });
+	
 }
