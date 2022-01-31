c7ff20b5e6abdfac306a3089eb3f47b63b50e155
JosiahFuller <josiahfullervgdesign@gmail.com>
2022-01-31 09:40:15 -0800

    Spencer - Function "Play Sound 1": Stable

--- Haunted Mansion/Haunted Mansion Program/Haunted Mansion.js
+++ Haunted Mansion/Haunted Mansion Program/Haunted Mansion.js
@@ -6,15 +6,24 @@ Program: We will be making a program to go through a haunted mansion
  filled with haunted sounds, spooky color changes, and other obstacles
 */
 
-async function startProgram()
-{
+async function startProgram(){
+	//Calls the individual functions
 	await goToBlueLight();
-	
+	await goSound1();
 }
 
-async function goToBlueLight()
-{
+async function goToBlueLight(){
+	//Display the Start message
 	await scrollMatrixText('Start', {r: 255, g: 0, b: 0}, 30, true);
-	await roll(0, 100, 1.45);
+	//Roll to the blue light
+	await roll(0, 50, 3.1);
+	//Sets the light to blue
 	await setMainLed({ r: 0, g: 0, b: 255 });
+}
+
+async function goSound1(){
+	await delay(0.5);
+	//Rolls to the sound icon and plays a sound
+	await roll(90, 50, 2.1);
+	await Sound.Mechanical.Ambulance.play(true);
 }
